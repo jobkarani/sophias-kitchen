030f77d5d36744e1c5a856c86cc7a8f6944e909e
Job Karani <moringa@moringa-Thinkpad-L450>
2022-01-02 12:07:41 +0300

    makes initial migrations

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 import datetime as dt
 from tinymce.models import HTMLField
+
 # Create your models here.
 
 
@@ -19,19 +20,91 @@ class User(models.Model):
         return self.name
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=256, null=True)
+    email = models.EmailField(max_length=256, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Product(models.Model):
     # category = models.ManyToManyField(Category, related_name='products')
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
     image = CloudinaryField('image')
     description = models.TextField()
-    price = models.IntegerField()
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
     status = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('name',)
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=256, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+
+        for item in orderitems:
+            if item.product.digital == False:
+                shipping = True
+
+        return shipping
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+
+# class ShippingAddress(models.Model):
+#     customer = models.ForeignKey(
+#         Customer, on_delete=models.SET_NULL, null=True, blank=True)
+#     order = models.ForeignKey(
+#         Order, on_delete=models.SET_NULL, null=True, blank=True)
+#     address = models.CharField(max_length=256, null=True)
+#     city = models.CharField(max_length=256, null=True)
+#     state = models.CharField(max_length=256, null=True)
+#     zipcode = models.CharField(max_length=256, null=True)
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.address
