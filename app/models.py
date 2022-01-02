9de65e9b28b087536ad312cd4d25a168d0929c3e
Job Karani <moringa@moringa-Thinkpad-L450>
2022-01-02 05:25:23 +0300

    user and products model created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+import datetime as dt
+from tinymce.models import HTMLField
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    profile_pic = CloudinaryField('image')
+    email = models.EmailField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    # category = models.ManyToManyField(Category, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = CloudinaryField('image')
+    description = models.TextField()
+    price = models.IntegerField()
+    status = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
--- project/settings.py
+++ project/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sophie',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -120,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
