e01d8b930dc739b88fc2ed0d544dcb44c34977e2
Job Karani <jobkarani084j@gmail.com>
2022-03-09 14:03:24 +0300

    creates subtotal method in cartitem model

--- app/views.py
+++ app/views.py
@@ -152,10 +152,21 @@ def add_cart(request, product_id):
             cart = cart,
         )
         cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()
+    
     return redirect('cart')
 
-def cart(request): 
+def cart(request, total=0, quantity=0, cart_items=None): 
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price*cart_item*quantity)
+    except Exception as e:
+        pass
 
-    return render(request, 'all-temps/cart.html')
+    ctx = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items
+    }
+    return render(request, 'all-temps/cart.html', ctx)
