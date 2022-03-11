103282374d7b0073f479536f0dca6b5e6b7d71cc
Job Karani <jobkarani084j@gmail.com>
2022-03-11 15:44:41 +0300

    updates product detail funtion

--- app/views.py
+++ app/views.py
@@ -116,11 +116,14 @@ def product_detail(request, category_slug, product_slug):
 
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product )
+        
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart':in_cart
     }
     return render(request, 'all-temps/product.html',context)
 
