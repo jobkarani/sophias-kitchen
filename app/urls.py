ee88134890740e5c39e4d791dcfb48aa03f4770d
Job Karani <jobkarani084j@gmail.com>
2022-03-11 14:49:04 +0300

    creates remove_cart_item function

--- app/urls.py
+++ app/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
     path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
+    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
     path('aboutus/', views.About, name='About'),
 ]
--- app/views.py
+++ app/views.py
@@ -168,6 +168,13 @@ def remove_cart(request, product_id):
     
     return redirect('cart')
 
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    cart_item.delete()
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None): 
     try:
