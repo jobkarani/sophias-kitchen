d5b842ae418194b5ae67d75b89e1d5ca9410bd3e
Job Karani <jobkarani084j@gmail.com>
2022-03-27 18:36:59 +0300

    fixes navbar login/register bugs

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# redirect urls
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -137,6 +139,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
