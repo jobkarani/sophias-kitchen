ea63e639f358a0137e6c2e030605e2a147f91890
Job Karani <jobkarani084j@gmail.com>
2022-04-18 05:01:53 +0300

    creates mpesa model

--- app/models.py
+++ app/models.py
@@ -220,3 +220,19 @@ class OrderProduct(models.Model):
 
     def __str__(self):
         return self.product.name
+
+
+class MpesaPayment(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    type = models.TextField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    contact = models.TextField()
+
+    class Meta:
+        verbose_name = "Mpesa Payment"
+        verbose_name_plural = "Mpesa Payments"
+
+    def __str__(self):
+        return self.first_name
