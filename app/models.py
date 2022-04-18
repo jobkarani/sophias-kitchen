dd07341d6eb9fc3f12d4bbb2334ca606e120aeeb
Job Karani <jobkarani084j@gmail.com>
2022-04-18 05:10:17 +0300

    creates model payment

--- app/models.py
+++ app/models.py
@@ -221,6 +221,11 @@ class OrderProduct(models.Model):
     def __str__(self):
         return self.product.name
 
+class Payment(models.Model):
+    first_name = models.CharField(max_length=144, null=True, blank=True)
+    last_name = models.CharField(max_length=144, null=True, blank=True)
+    contact = models.CharField(max_length=30)
+
 
 class MpesaPayment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
